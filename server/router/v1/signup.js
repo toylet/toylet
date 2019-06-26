b7cd4ae7f09aec26259e7db38a0871fa38f613fd
unknown <mycool0905@gmail.com>
2019-06-27 04:59:55 +0900

    remove middleware express-basic-auth

--- server/router/v1/signup.js
+++ server/router/v1/signup.js
@@ -1,10 +1,5 @@
 const axios = require('axios');
-const express = require('express');
-const router = express.Router();
 const model = require('./../../model');
-
-
-const app = express();
 
 module.exports = () => {
     router.post('/', async (req, res) => {
