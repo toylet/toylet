2af2daed495c03d6e84139468d52662ec1542a18
unknown <mycool0905@gmail.com>
2019-06-27 04:11:21 +0900

    user_schema

--- server/lib/signup.js
+++ server/lib/signup.js
@@ -1,10 +1,17 @@
 const axios = require('axios');
-
+const basicAuth = require('express-basic-auth');
+const UserSchema = require('./../model/user_schema');
 
 //TODO(Taeyoung) : chanage var name
-const signUp = async (re) =>{
-    //TODO(Taeyoung) : implement signup function here
+const signUp = async (req) =>{
+    const userMatches = basicAuth.safeCompare(req.userEmail, 'email');
 
+    if(!userMatches){
+        console.log('이미 아이디가 존재함');
+    }
+    else{
+        
+    }
 };
 
 module.exports={
