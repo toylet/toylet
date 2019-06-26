2b0f202b3c3bb946d7921563b8413333911b7406
Jelldo <astrokwak@gmail.com>
2019-06-27 07:39:41 +0900

    add profile picture

--- server/router/v1/signup.js
+++ server/router/v1/signup.js
@@ -1,7 +1,25 @@
+// const model = require('../../model');
+const express = require('express');
+const router = express.Router();
 const axios = require('axios');
+const multer = require('multer');
+const UPLOAD_DIR = './server/uploads/profile';
 
-module.exports = () => {
-    router.post('/', async (req, res) => {
-        //TODO(Taeyoung) : use signup function here
-    });
-};
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, UPLOAD_DIR);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const profileImageUpload = multer({ storage });
+
+router.post('/', profileImageUpload.single('profileImage'), (req, res) => {
+    res.json({ msg: 'upload success' });
+    console.log(req.file.path); // file path
+    //TODO(Taeyoung) : Save file with user data here
+});
+
+module.exports = router;
