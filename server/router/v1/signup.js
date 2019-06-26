788ea29039b4b7d0f6ad137ca9933d632e1c3a92
unknown <mycool0905@gmail.com>
2019-06-27 04:58:56 +0900

    remove express-basic-auth from signup.js

--- server/router/v1/signup.js
+++ server/router/v1/signup.js
@@ -1,6 +1,10 @@
+const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 const model = require('./../../model');
+
+
+const app = express();
 
 module.exports = () => {
     router.post('/', async (req, res) => {
