5df2073d82f396b7b5ae53520433a18d811cf5e5
Jelldo <astrokwak@gmail.com>
2019-06-27 15:44:08 +0900

    create-UserProjectMapsSchema

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     email : {type:String, index:"hashed", required:true, unique:true},
-    password : {type:String, type:String, required:true},
+    password : {type:String, required:true},
     name : {type:String, required: true},
-    average_working_time : [{type:Date}],
     job : {type:String, 'default':''},
     company : {type:String, 'default':''},
     language : [{type:String, 'default':'English'}],
@@ -13,7 +12,11 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.path('email').validate((email)=>{
-    return email.lenghth;
+    return email.length;
+}, 'have no email');
+
+UserSchema.path('password').validate((password)=>{
+    return password.length;
 }, 'have no email');
 
 const UserModel = mongoose.model('users', UserSchema);
