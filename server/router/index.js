9ffafad15b5e8c2460b51498cdddae32df3ca3cb
Jelldo <astrokwak@gmail.com>
2019-06-27 16:40:31 +0900

    modift user API

--- server/router/index.js
+++ server/router/index.js
@@ -4,16 +4,18 @@ const config = require('./../config/config.json');
 const version = config.version;
 
 module.exports = () => {
-    const signUp = require('./' + version + '/signup')();
-    const signIn = require('./' + version + '/signin')();
+    const user = require('./' + version + '/user')();
+    // const signUp = require('./' + version + '/signup')();
+    // const signIn = require('./' + version + '/signin')();
     const project = require('./' + version + '/project')();
     router.use((req, res, next) => {
         next();
     });
 
     //TODO(Taeyoung): change user API
-    router.use('/' + version + '/signup', signUp);
-    router.use('/' + version + '/signin', signIn);
+    router.use('/' + version + '/user', user);
+    // router.use('/' + version + '/signup', signUp);
+    // router.use('/' + version + '/signin', signIn);
     router.use('/' + version + '/project', project);
 
     return router;
