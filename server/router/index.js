aa52cd458264971ea83ff4deff9acbb209d70215
Jelldo <astrokwak@gmail.com>
2019-06-27 16:13:06 +0900

    add-project-query

--- server/router/index.js
+++ server/router/index.js
@@ -6,12 +6,15 @@ const version = config.version;
 module.exports = () => {
     const signUp = require('./' + version + '/signup')();
     const signIn = require('./' + version + '/signin')();
+    const project = require('./' + version + '/project')();
     router.use((req, res, next) => {
         next();
     });
 
+    //TODO(Taeyoung): change user API
     router.use('/' + version + '/signup', signUp);
     router.use('/' + version + '/signin', signIn);
+    router.use('/' + version + '/project', project);
 
     return router;
 }
