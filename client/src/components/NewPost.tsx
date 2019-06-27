951da8c9fe83a75c5f080bdc2b3c5f2888fe4e0d
hyperthreading <no1expman@gmail.com>
2019-06-27 13:39:18 +0900

    Change the fonts and color of the editor and temporarily saving the content to the state

--- client/src/components/NewPost.tsx
+++ client/src/components/NewPost.tsx
@@ -1,12 +1,92 @@
 import * as React from 'react';
-import Editor from 'rich-markdown-editor';
+import Editor, { OnChange, Theme } from 'rich-markdown-editor';
+
+import styles from './NewPost.module.scss';
 
 const markdownTest = `
 # Header 1
 ## Header 2
+
+asdfasd
+fasdf
+asdf
 `;
 
-export default class NewPost extends React.Component {
+const colors = {
+    almostBlack: '#181A1B',
+    lightBlack: '#2F3336',
+    almostWhite: '#E6E6E6',
+    white: '#FFF',
+    white10: 'rgba(255, 255, 255, 0.1)',
+    black: '#000',
+    black10: 'rgba(0, 0, 0, 0.1)',
+    primary: '#1AB6FF',
+    greyLight: '#F4F7FA',
+    grey: '#E8EBED',
+    greyMid: '#9BA6B2',
+    greyDark: '#DAE1E9'
+};
+
+export const base = {
+    ...colors,
+    fontFamily: "'Source Sans Pro', sans-serif;",
+    fontFamilyMono:
+        "'SFMono-Regular',Consolas,'Liberation Mono', Menlo, Courier,monospace",
+    fontWeight: 400,
+    link: colors.primary,
+    placeholder: '#B1BECC',
+    textSecondary: '#4E5C6E',
+    textLight: colors.white,
+    selected: colors.primary,
+    codeComment: '#6a737d',
+    codePunctuation: '#5e6687',
+    codeNumber: '#d73a49',
+    codeProperty: '#c08b30',
+    codeTag: '#3d8fd1',
+    codeString: '#032f62',
+    codeSelector: '#6679cc',
+    codeAttr: '#c76b29',
+    codeEntity: '#22a2c9',
+    codeKeyword: '#d73a49',
+    codeFunction: '#6f42c1',
+    codeStatement: '#22a2c9',
+    codePlaceholder: '#3d8fd1',
+    codeInserted: '#202746',
+    codeImportant: '#c94922'
+};
+
+export const light: Theme = {
+    ...base,
+    background: colors.white,
+    text: '#0c1c2d',
+    code: colors.lightBlack,
+
+    toolbarBackground: colors.lightBlack,
+    toolbarInput: colors.white10,
+    toolbarItem: colors.white,
+
+    blockToolbarBackground: colors.greyLight,
+    blockToolbarTrigger: colors.greyMid,
+    blockToolbarTriggerIcon: colors.white,
+    blockToolbarItem: colors.almostBlack,
+
+    tableDivider: colors.grey,
+    tableSelected: colors.primary,
+    tableSelectedBackground: '#E5F7FF',
+
+    quote: colors.greyDark,
+    codeBackground: colors.greyLight,
+    codeBorder: colors.grey,
+    horizontalRule: colors.grey,
+    imageErrorBackground: colors.greyLight
+};
+
+export default class NewPost extends React.Component<{}, { v: string }> {
+    // TODO:: debounce
+    onChange: OnChange = value => {
+        this.setState({ v: value() });
+    };
+
     render() {
         return (
             <div>
@@ -14,9 +94,12 @@ export default class NewPost extends React.Component {
                     <input placeholder="Title" />
                     <span>Save</span>
                 </div>
-                <div>EDITOR 자리임</div>
-                <div style={{ margin: 50 }}>
-                    <Editor defaultValue={markdownTest} />
+                <div className={styles.editor} style={{ margin: 50 }}>
+                    <Editor
+                        onChange={this.onChange}
+                        defaultValue={markdownTest}
+                        theme={light}
+                    />
                 </div>
             </div>
         );
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -6,50 +6,62 @@ interface ITokenProps {
 
 declare module 'rich-markdown-editor' {
     interface Props {
-        defaultValue: String;
+        defaultValue?: String;
+        theme?: Theme;
+        onSave?: ({ done: boolean }) => void;
+        onChange?: OnChange;
     }
 
+    export type OnChange = (value: () => string) => void;
+
     export interface Theme {
-        almostBlack: string,
-        lightBlack: string,
-        almostWhite: string,
-        white: string,
-        white10: string,
-        black: string,
-        black10: string,
-        primary: string,
-        greyLight: string,
-        grey: string,
-        greyMid: string,
-        greyDark: string,
+        code: string;
+        tableDivider: string;
+        tableSelected: string;
+        tableSelectedBackground: string;
 
-        fontFamily: string,
-        fontWeight: number | string,
-        link: string,
-        placeholder: string,
-        textSecondary: string,
-        textLight: string,
-        selected: string,
+        imageErrorBackground: string;
 
-        background: string,
-        text: string,
+        almostBlack: string;
+        lightBlack: string;
+        almostWhite: string;
+        white: string;
+        white10: string;
+        black: string;
+        black10: string;
+        primary: string;
+        greyLight: string;
+        grey: string;
+        greyMid: string;
+        greyDark: string;
 
-        toolbarBackground: string,
-        toolbarInput: string,
-        toolbarItem: string,
+        fontFamily: string;
+        fontWeight: number | string;
+        link: string;
+        placeholder: string;
+        textSecondary: string;
+        textLight: string;
+        selected: string;
 
-        blockToolbarBackground: string,
-        blockToolbarTrigger: string,
-        blockToolbarTriggerIcon: string,
-        blockToolbarItem: string,
+        background: string;
+        text: string;
 
-        quote: string,
-        codeBackground?: string,
-        codeBorder?: string,
-        horizontalRule: string,
+        toolbarBackground: string;
+        toolbarInput: string;
+        toolbarItem: string;
 
-        hiddenToolbarButtons?: HiddenToolbarButtons,
-    };
+        blockToolbarBackground: string;
+        blockToolbarTrigger: string;
+        blockToolbarTriggerIcon: string;
+        blockToolbarItem: string;
+
+        quote: string;
+        codeBackground?: string;
+        codeBorder?: string;
+        horizontalRule: string;
+
+        hiddenToolbarButtons?: HiddenToolbarButtons;
+    }
 
     export default class Editor extends React.Component<Props> {}
 }
