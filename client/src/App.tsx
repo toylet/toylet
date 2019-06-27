5aa40c30b7f726f6926bb298851da778a76c58a4
Muhun Kim <iam@muhun.kim>
2019-06-28 00:30:11 +0900

    delete main component import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ import {
     Redirect
 } from 'react-router-dom';
 import WebFont from 'webfontloader';
-import Login from './components/Login';
 import Discover from './components/Discover';
 import ProjectDetail from './components/ProjectDetail';
 import Signup from './components/Signup/';
