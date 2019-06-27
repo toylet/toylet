a893e611dc6e0678844989be891a3ada80b72f99
unknown <mycool0905@gmail.com>
2019-06-27 13:16:35 +0900

    add signup

---
+++ server/models/user_model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    email : {type:String, index:"hashed", required:true, unique:true},
+    password : {type:String, type:String, required:true},
+    name : {type:String, required: true},
+    average_working_time : [{type:Date}],
+    job : {type:String, 'default':''},
+    company : {type:String, 'default':''},
+    language : [{type:String, 'default':'English'}],
+    link : {type:String},
+    profileImageLink : {type:String}
+});
+
+UserSchema.path('email').validate((email)=>{
+    return email.lenghth;
+}, 'have no email');
+
+const UserModel = mongoose.model('users', UserSchema);
+
+module.exports = UserModel;
