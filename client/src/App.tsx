4211cf82cc20f6e174d1774f619f51d278712c5d
hyperthreading <no1expman@gmail.com>
2019-06-27 10:18:25 +0900

    Add HOC routers and discover routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import styles from './App.module.scss';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    RouteProps,
+    Redirect
+} from 'react-router-dom';
 import Main from './components/Main';
 import Login from './components/Login';
+import Discover from './components/Discover';
 
 class App extends Component<{}, {}> {
     render() {
@@ -11,15 +18,62 @@ class App extends Component<{}, {}> {
             <div className={styles.App}>
                 <Router>
                     <Switch>
-                        <Route
-                            exact
+                        <AuthRoute
                             path="/login"
-                            render={() => <Login token={token} />}
+                            component={Login}
+                            token={token}
                         />
-                        <Route path="/" render={() => <Main token={token} />} />
+                        <PrivateRoute
+                            path="/discover"
+                            component={Discover}
+                            token={token}
+                        />
+                        <PrivateRoute path="/" component={Main} token={token} />
                     </Switch>
                 </Router>
             </div>
+        );
+    }
+}
+
+class AuthRoute<P extends RouteProps = RouteProps> extends Component<
+    P & ITokenProps
+> {
+    render() {
+        const { token, component: Comp, ...rest } = this.props;
+        return (
+            <Route
+                {...rest}
+                render={props => {
+                    return token ? (
+                        <Redirect to={'/login'} />
+                    ) : (
+                        // @ts-ignore
+                        <Comp {...props} />
+                    );
+                }}
+            />
+        );
+    }
+}
+
+class PrivateRoute<P extends RouteProps = RouteProps> extends Component<
+    P & ITokenProps
+> {
+    render() {
+        const { token, component: Comp, ...rest } = this.props;
+        return (
+            <Route
+                {...rest}
+                render={props => {
+                    return !token ? (
+                        <Redirect to={'/login'} />
+                    ) : (
+                        // @ts-ignore
+                        <Comp {...props} />
+                    );
+                }}
+            />
         );
     }
 }
--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { Redirect } from 'react-router';
+
+import styles from './Main.module.scss';
 
 export default class Main extends Component<ITokenProps> {
-    constructor(props: ITokenProps) {
-        super(props);
-    }
-
 
     onClickLogout = () => {
         localStorage.removeItem('token');
@@ -15,15 +12,13 @@ export default class Main extends Component<ITokenProps> {
 
     render() {
         return (
-            <div>
-                {!this.props.token ? (
-                    <Redirect to={'/login'} />
-                ) : (
-                    <div>
-                        Wow login1!
-                        <button onClick={this.onClickLogout}>logout</button>
-                    </div>
-                )}
+            <div className={styles.container}>
+                <div className={styles.sidebar}>
+                    <button onClick={this.onClickLogout}>logout</button>
+                    <div>My Projects</div>
+                    <button>Discover</button>
+                </div>
+                <div className={styles.projects}>wef</div>
             </div>
         );
     }
